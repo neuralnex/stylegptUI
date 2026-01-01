3c54b0e80b3588f5bdd64b029cf104a04ecff2fc
neuralnex <omezirizion@gmail.com>
2026-01-01 14:47:32 -0600

    Homepage changes to Footer

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -3,8 +3,6 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Service from "../components/Service";
 import About from "../components/About";
-import Brands from "../components/Brands";
-import Contact from "../components/Contact";
 
 const Home = () => {
   return (
@@ -13,8 +11,6 @@ const Home = () => {
       <Hero />
       <Service />
       <About />
-      <Brands />
-      <Contact />
     </div>
   );
 };
